e32731f6af06d87ff1883bdb10d15f07d3f16bd7
saifullah-10 <saifullah.bus@gmail.com>
2024-06-07 19:07:52 +0600

    initial server setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+// middleware
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => console.log("listening on port", port));
